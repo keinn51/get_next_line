f18c3898037a24827da84d1679e5f0be759a2330
keinn51 <soogood97@gmail.com>
2022-03-31 00:16:16 +0900

    Add README & Edit folder structure
    -

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyungsle <kyungsle@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/01 12:23:56 by kyungsle          #+#    #+#             */
+/*   Updated: 2022/02/03 15:37:11 by kyungsle         ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	int				fd;
+	char			*content;
+	struct s_list	*next;
+}	t_list;
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strndup(const char *s1, size_t n);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_lstdel(t_list **head_ptr, t_list *curr_ptr);
+
+#endif
